779b65112f994431c7fe34a9a5300a8bb678b000
kabeerx9 <kabeer786joshi@gmail.com>
2023-12-18 17:52:39 +0530

    practice-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import ReactErrorBoundary from './components/error-handling/usingErrorBoundaries';
+import Practice1 from './interview-challenge/Practice1';
 
 const App = () => {
   return (
     <>
       <div className="h-screen w-screen bg-gray-200">
-        <ReactErrorBoundary />
+        <Practice1 />
       </div>
     </>
   );
---
+++ src/interview-challenge/Practice1.tsx
@@ -0,0 +1,51 @@
+import { Button } from '@/components/ui/button';
+import { useState } from 'react';
+
+const temp1 = ['apples', 'bananas', 'oranges', 'grapes', 'pears', 'pineapple'];
+const temp2 = ['potatoes', 'beans', 'carrots', 'spinnach', 'kale', 'broccoli'];
+
+const Practice1 = () => {
+  // button , and on click swap only the odd item from cartA , with odd items in cart B
+
+  const [cartA, setCartA] = useState(temp1);
+  const [cartB, setCartB] = useState(temp2);
+
+  const handleSwap = () => {
+    const newCartA = cartA.map((item, index) => {
+      if (index % 2 !== 0) {
+        return cartB[index];
+      }
+      return item;
+    });
+    const newCartB = cartB.map((item, index) => {
+      if (index % 2 !== 0) {
+        return cartA[index];
+      }
+      return item;
+    });
+    setCartA(newCartA);
+    setCartB(newCartB);
+  };
+
+  return (
+    <div className="flex justify-center items-center p-4 text-2xl gap-8">
+      <div className="flex flex-col gap-4 border-2 border-black p-4 rounded-lg">
+        {cartA.map((item, index) => (
+          <div key={index}>
+            {index + 1} : {item}
+          </div>
+        ))}
+      </div>
+      <Button onClick={handleSwap}>Swap</Button>
+      <div className="flex flex-col gap-4 border-2 border-black p-4 rounded-lg">
+        {cartB.map((item, index) => (
+          <div key={index}>
+            {index + 1} : {item}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Practice1;
