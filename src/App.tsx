3f4a544ef33e2e831f3146ab29ce656ed7009d27
kabeerx9 <kabeer786joshi@gmail.com>
2023-12-25 21:03:42 +0530

    confirm modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { toast } from 'sonner';
+import { ConfirmModal } from './components/modals/confirm-modal';
 import ToastSonner from './components/shad-cn/toast-sonner';
+import { Button } from './components/ui/button';
 
 const App = () => {
   return (
     <div className="h-screen w-screen bg-gray-200">
       <ToastSonner />
+      <ConfirmModal
+        onConfirm={() => toast.success('Confirm modal working fine ')}
+      >
+        <Button>Open Alert</Button>
+      </ConfirmModal>
     </div>
   );
 };
