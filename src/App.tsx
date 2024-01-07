8221620403bb68b2d81d7cd729c27f029cb1bbc9
kabeerx9 <kabeer786joshi@gmail.com>
2024-01-07 12:25:16 +0530

    very simple one

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import { useEffect, useState } from 'react';
-import Practice5 from './interview-challenge/Practice5';
-
-const TIME = 30;
+import Practice6 from './interview-challenge/Practice6';
 
 const App = () => {
-  const [seconds, setSeconds] = useState(TIME);
-  useEffect(() => {
-    const i = setInterval(() => {
-      setSeconds((s) => {
-        if (s === 0) {
-          clearInterval(i);
-          return 0;
-        }
-        return s - 1;
-      });
-    }, 1000);
-
-    return () => clearInterval(i);
-  }, []);
-
   return (
     <div className="h-screen w-screen">
-      <Practice5 size={200} timeRemaining={seconds} totalTime={TIME} />
+      <Practice6 />
     </div>
   );
 };
