1c00cf275702ef29ad2f2108e18afd2f12581e2c
kabeerx9 <kabeer786joshi@gmail.com>
2024-05-29 20:39:58 +0530

    adding script

--- git_auto_push.sh
+++ git_auto_push.sh
@@ -3,8 +3,13 @@
 # Add all changes
 git add .
 
-# Commit with a message
-git commit -m "small fix"
+# Prompt the user for a commit message
+echo "Enter commit message:"
+read commit_message
+
+# Commit with the user's message
+git commit -m "$commit_message"
 
 # Push changes to the remote repository
 git push
+
